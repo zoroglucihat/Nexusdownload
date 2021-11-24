4a3006b93c9fb48afe0c5543436c85af2d4dcae1
Cihat Zoroğlu <65086293+zorolos@users.noreply.github.com>
2021-11-24 16:03:46 +0300

    Update nexussonatypedownload.sh

--- nexussonatypedownload.sh
+++ nexussonatypedownload.sh
@@ -3,5 +3,5 @@
 links=$(curl -u user:pass -X GET 'http://1ip:8081/service/rest/v1/search/assets?repository=test&group=' | grep downloadUrl | cut -d ":" -f 2-4 | cut -d "," -f1 | cut -d'"' -f 2)
 
 for link in $links; do
-    curl -X GET -u user:pass http://ip:8081/repository/rpm/3.0.0/${link} -O
+    curl -X GET -u user:pass http://ip:8081/repository/rpm/${link} -O
 done
