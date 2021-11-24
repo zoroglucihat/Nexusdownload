4ce686067e72c0dbe248dc097b7a669a3f464610
Cihat Zoroğlu <65086293+zorolos@users.noreply.github.com>
2021-11-24 10:47:53 +0300

    Update nexussonatypedownload.sh

--- nexussonatypedownload.sh
+++ nexussonatypedownload.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+#Author:    Cihat Zoroğlu
 links=$(curl -u user:pass -X GET 'http://1ip:8081/service/rest/v1/search/assets?repository=test&group=' | grep downloadUrl | cut -d ":" -f 2-4 | cut -d "," -f1 | cut -d'"' -f 2)
 
 for link in $links; do
