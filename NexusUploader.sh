3eb9b0337c345ca2575bb90a87b5abf81de8ce4c
Cihat Zoroğlu <65086293+zorolos@users.noreply.github.com>
2021-11-29 20:42:01 +0300

    Update NexusUploader.sh

--- NexusUploader.sh
+++ NexusUploader.sh
@@ -1,4 +1,5 @@
   #!/bin/bash
+  # This script is looking inside the folder and upload to the nexus
 	for d in *; do
 	curl -v -u admin:password --upload-file $d \
     http://<ip>:8081/repository/reponame/ \
